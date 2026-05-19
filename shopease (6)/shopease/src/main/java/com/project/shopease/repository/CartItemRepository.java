121f56fdcbb6e1987ddeba2dd99a4403dafd9e9c
Hariprasadh <146912249+Hari3167924@users.noreply.github.com>
2026-05-19 13:07:41 +0530

    Delete shopease (6)/shopease directory

--- shopease (6)/shopease/src/main/java/com/project/shopease/repository/CartItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.shopease.repository;
-
-import com.project.shopease.entity.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-}
